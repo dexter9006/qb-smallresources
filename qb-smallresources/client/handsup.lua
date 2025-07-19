c901732bdb36c28196c1d321c4ee3b068f922b3b
dexter9006 <52006237+dexter9006@users.noreply.github.com>
2025-07-19 23:11:47 +0100

    Add files via upload

---
+++ qb-smallresources/client/handsup.lua
@@ -0,0 +1,24 @@
+local handsUp = false
+
+RegisterCommand(Config.HandsUp.command, function()
+    local ped = PlayerPedId()
+    if not HasAnimDictLoaded('missminuteman_1ig_2') then
+        RequestAnimDict('missminuteman_1ig_2')
+        while not HasAnimDictLoaded('missminuteman_1ig_2') do
+            Wait(10)
+        end
+    end
+    handsUp = not handsUp
+    if exports['qb-policejob']:IsHandcuffed() or PlayerData.metadata["isdead"] or PlayerData.metadata["inlaststand"] then return end --vitto add multiple states
+    if handsUp then
+        TaskPlayAnim(ped, 'missminuteman_1ig_2', 'handsup_base', 8.0, 8.0, -1, 50, 0, false, false, false)
+        --TaskPlayAnim(ped, 'missminuteman_1ig_2', 'handsup_base', 2.0, 2.5, -1, 50, 0, false, false, false) -- c'est ce qu'on avait dans la backup
+        exports['qb-smallresources']:addDisableControls(Config.HandsUp.controls)
+    else
+        ClearPedTasks(ped)
+        exports['qb-smallresources']:removeDisableControls(Config.HandsUp.controls)
+    end
+end, false)
+
+RegisterKeyMapping(Config.HandsUp.command, 'Hands Up', 'keyboard', Config.HandsUp.keybind)
+exports('getHandsup', function() return handsUp end)
